74ff5a4a64eab776d933f3511c4121cd5b920b4d
Debargha Chakraborty <chakrabortydebargha22@gmail.com>
2026-06-10 10:35:49 +0530

    Add: is-subsequence - submission-7

---
+++ Data Structures & Algorithms/is-subsequence/submission-7.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int si=0,ti=0;
+        int check=0;
+        while(si<s.length() && ti<t.length()){
+            if (s.charAt(si)==t.charAt(ti)){
+                si++;
+                check++;
+            }
+            ti++;
+        }
+        if(check==s.length()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
